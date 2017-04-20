d574b5d94e045a4ad6fd65cf8ba04f667f02d04c
Nick Willett-Jeffries <nwillettjeffries@gmail.com>
2017-04-19 23:33:37 -0400

    Add random quote generation to the landing page

--- main.go
+++ main.go
@@ -11,13 +11,14 @@ import (
 	"math/rand"
 	"net/http"
 	"time"
+	"strconv"
 )
 
 func main() {
 	sectionData := initSectionData()
 	router := httprouter.New()
 	router.ServeFiles("/assets/*filepath", http.Dir("assets"))
-	router.GET("/", IndexHandler)
+	router.GET("/", ContextDecorator(IndexHandler, sectionData))
 	router.GET("/rand", ContextDecorator(RandomHandler, sectionData))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
@@ -46,6 +47,23 @@ func initSectionData() []section {
 	return sectionData
 }
 
+func getRandomSection(sectionData []section, maxLength int) section {
+	if maxLength >= 0 {
+		var filteredSections []section
+		for _, s := range(sectionData) {
+			if s.Length <= maxLength {
+				filteredSections = append(filteredSections, s)
+			}
+		}
+		sectionData = filteredSections
+	}
+
+	seed := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(seed)
+	random := rng.Intn(len(sectionData))
+	return sectionData[random]
+}
+
 func ContextDecorator(h httprouter.Handle, sectionData []section) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		ctx := context.WithValue(r.Context(), "sectionData", sectionData)
@@ -54,22 +72,30 @@ func ContextDecorator(h httprouter.Handle, sectionData []section) httprouter.Han
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	sectionData := r.Context().Value("sectionData").([]section)
+	section := getRandomSection(sectionData, 350)
 	t, err := template.ParseFiles("views/index.html")
 	if err != nil {
 		panic(err)
 	}
-	t.Execute(w, nil)
+	t.Execute(w, &section)
 }
 
-func RandomHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	sectionData := r.Context().Value("sectionData").([]section)
+func RandomHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	maxLength := -1
+	if len(r.URL.Query()["maxLength"]) == 1 {
+		maxLengthQueryParam, err := strconv.Atoi(r.URL.Query()["maxLength"][0])
+		if err != nil {
+			panic(err)
+		}
+		maxLength = maxLengthQueryParam
+	}
 
-	seed := rand.NewSource(time.Now().UnixNano())
-	rng := rand.New(seed)
-	random := rng.Intn(len(sectionData))
-	jsonSection, err := json.MarshalIndent(sectionData[random], "", "    ")
+	sectionData := r.Context().Value("sectionData").([]section)
+	jsonSection, err := json.MarshalIndent(getRandomSection(sectionData, maxLength), "", "    ")
 	if err != nil {
 		panic(err)
 	}
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprint(w, string(jsonSection))
 }
